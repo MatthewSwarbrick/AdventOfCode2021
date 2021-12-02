43500a24904918a8f35886740dd5aa98c1df88a5
Matt <matt_swarbrick@hotmail.com>
2021-12-02 13:23:09 +0000

    complete day1 puzzle1

--- Day1/src/main/kotlin/Main.kt
+++ Day1/src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Hello World!")
+    val input = puzzle1
+
+    val totalIncreases = input.reduceIndexed { index, acc, curr ->
+        if(curr > input[index - 1]) {
+                acc + 1
+        } else {
+            acc
+        }
+    }
+
+    print(totalIncreases - input[0])
 }
