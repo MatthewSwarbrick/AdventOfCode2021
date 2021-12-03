6c1772eaa236d2e98b8ba101cb9578eac20ede6f
Matt Swarbrick <matt@invevo.com>
2021-12-03 08:15:44 +0000

    set up empty proj for day3

---
+++ Day3/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    solvePart1()
+    solvePart2()
+}
+
+private fun solvePart1() {
+    println("Solution to part1:")
+}
+
+private fun solvePart2() {
+    println("Solution to part2:")
+}
---
+++ Day3/src/main/kotlin/inputs.kt
@@ -0,0 +1,2 @@
+//val puzzle = listOf(
+//)
