fbe05abbdfc7951d22c10fcba83abcba729760f6
Matt Swarbrick <matt@invevo.com>
2021-12-03 08:36:23 +0000

    solve day3 part1

--- Day3/src/main/kotlin/Main.kt
+++ Day3/src/main/kotlin/Main.kt
@@ -4,9 +4,36 @@ fun main() {
 }
 
 private fun solvePart1() {
-    println("Solution to part1:")
+    val reportLength = puzzle[0].length
+
+    var finalGamma = ""
+    var finalEpsilon = ""
+    for (i in 0 until reportLength) {
+        val groups = puzzle.map { it[i] }.groupBy { it }
+        val gamma = groups.maxByOrNull { it.value.count() }!!.key
+        val epsilon = groups.minByOrNull { it.value.count() }!!.key
+        finalGamma += gamma
+        finalEpsilon += epsilon
+    }
+
+    val answer = finalGamma.toLong(2) * finalEpsilon.toLong(2)
+    println("Solution to part1: $answer")
 }
 
 private fun solvePart2() {
+//    val input = listOf(
+//        "00100",
+//        "11110",
+//        "10110",
+//        "10111",
+//        "10101",
+//        "01111",
+//        "00111",
+//        "11100",
+//        "10000",
+//        "11001",
+//        "00010",
+//        "01010"
+//    )
     println("Solution to part2:")
 }
