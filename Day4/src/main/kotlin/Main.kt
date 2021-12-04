78f602ce1c7b83d4e69a135ae5ee746269c4564c
matt <matt_swarbrick@hotmail.com>
2021-12-04 08:05:41 +0000

    set up day4

---
+++ Day4/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    solvePart1()
+    solvePart2()
+}
+
+private fun solvePart1() {
+    val answer = 0
+    println("Solution to part1: $answer")
+}
+
+private fun solvePart2() {
+    val answer = 0
+    println("Solution to part2: $answer")
+}
