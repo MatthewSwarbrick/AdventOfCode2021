7a2eb702f0f997c75e44c11f728f2a4d3b088bff
Matt Swarbrick <matt@invevo.com>
2021-12-07 08:49:27 +0000

    Day 7 - part 1 solution

--- Day7/src/main/kotlin/Main.kt
+++ Day7/src/main/kotlin/Main.kt
@@ -1,15 +1,29 @@
+import kotlin.math.abs
+
 fun main() {
     solvePart1()
     solvePart2()
 }
 
 private fun solvePart1() {
-    val answer = 0
+    val lowestPosition = puzzle.minOrNull()!!
+    val highestPosition = puzzle.maxOrNull()!!
 
+
+    val costs = (lowestPosition until highestPosition + 1).associateWith { position ->
+        puzzle.sumOf {
+            abs(it - position)
+        }
+    }
+
+    val answer = costs.minByOrNull { it.value }!!.value
     println("Solution to part1: $answer")
 }
 
 private fun solvePart2() {
+    val input = listOf(
+        16,1,2,0,4,2,7,1,2,14
+    )
     val answer = 0
 
     println("Solution to part2: $answer")
