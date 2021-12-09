7c72091c7dcac03f4425c8df162b6dad57ec5f4a
Matt <matt_swarbrick@hotmail.com>
2021-12-09 11:13:51 +0000

    Day 9 - part 2 solution

--- Day9/src/main/kotlin/Main.kt
+++ Day9/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import arrow.core.fold
+
 fun main() {
     solvePart1()
     solvePart2()
@@ -5,6 +7,7 @@ fun main() {
 
 private fun solvePart1() {
     val answer = puzzle
+        .toPoints()
         .toAdjacents()
         .toLowPoints()
         .toRiskLevel()
@@ -13,49 +16,92 @@ private fun solvePart1() {
 }
 
 private fun solvePart2() {
-    val input = listOf(
-        "2199943210",
-        "3987894921",
-        "9856789892",
-        "8767896789",
-        "9899965678"
-    )
+    val answer = puzzle
+        .toPoints()
+        .toAdjacents()
+        .toBasins()
+        .map { it.size }
+        .sortedDescending()
+        .take(3)
+        .fold(1) { acc, size -> acc * size }
 
-    val answer = 0
     println("Solution to part2: $answer")
 }
 
-private fun List<String>.toAdjacents(): MutableList<Adjacent> {
-    val xLength = this[0].length
-    val yLength = this.count()
-
-    val adjacents = mutableListOf<Adjacent>()
-
-    for(x in 0 until xLength) {
-        for(y in 0 until yLength) {
-            val heightPoint = this[y][x].digitToInt()
-            val top = if(y > 0) { this[y - 1][x] } else { null } ?.digitToInt() ?: 10
-            val left = if(x > 0) { this [y][x - 1] } else { null } ?.digitToInt() ?: 10
-            val bottom = if(y < yLength - 1) { this[y + 1 ][x] } else { null } ?.digitToInt() ?: 10
-            val right = if(x < xLength - 1) { this[y][x + 1] } else { null } ?.digitToInt() ?: 10
-
-            adjacents.add(Adjacent(heightPoint, top, left, bottom, right))
-        }
+private fun List<String>.toPoints() =
+     this.flatMapIndexed { y, row ->
+        row.map { it.digitToInt() }
+            .mapIndexed { x, height ->
+                Point(x, y, height)
+            }
     }
 
-    return adjacents
-}
+private fun List<Point>.toAdjacents(): List<Adjacent> =
+    this.map { point ->
+        val top = this.singleOrNull { it.x == point.x && it.y == point.y - 1 }
+        val bottom = this.singleOrNull { it.x == point.x && it.y == point.y + 1 }
+        val left = this.singleOrNull { it.x == point.x - 1 && it.y == point.y }
+        val right = this.singleOrNull { it.x == point.x + 1 && it.y == point.y }
+        Adjacent(point, top, left, bottom, right)
+    }
 
 private fun List<Adjacent>.toLowPoints() =
     this.filter {
-        it.heightPoint < it.top &&
-        it.heightPoint < it.left &&
-        it.heightPoint < it.bottom &&
-        it.heightPoint < it.right
+        (it.top == null || it.point.height < it.top.height) &&
+        (it.left == null || it.point.height < it.left.height) &&
+        (it.bottom == null || it.point.height < it.bottom.height) &&
+        (it.right == null || it.point.height < it.right.height)
     }
-    .map { it.heightPoint }
+        .map { it.point }
 
-private fun List<Int>.toRiskLevel() =
-    this.sumOf { it + 1 }
+private fun List<Point>.toRiskLevel() =
+    this.sumOf { it.height + 1 }
 
-data class Adjacent(val heightPoint: Int, val top: Int, val left: Int, val bottom: Int, val right: Int)
+private fun List<Adjacent>.toBasins(): List<Set<Point>> =
+    this.toLowPoints()
+    .map {
+        it.toBasin(this).plus(it).toSet()
+    }
+
+private fun Point.toBasin(adjacents: List<Adjacent>) : List<Point> {
+    val adjacent = adjacents.single { it.point == this }
+    val basinPoints = adjacent.toBasinPoints()
+    return basinPoints
+        .plus(basinPoints.flatMap { it.toBasin(adjacents) })
+}
+
+private fun Adjacent.toBasinPoints() : List<Point> {
+    val points = mutableListOf<Point>()
+    if(this.top.isBasinPoint(this.point.height)) {
+        points.add(this.top!!)
+    }
+
+    if(this.bottom.isBasinPoint(this.point.height)) {
+        points.add(this.bottom!!)
+    }
+
+    if(this.left.isBasinPoint(this.point.height)) {
+        points.add(this.left!!)
+    }
+
+    if(this.right.isBasinPoint(this.point.height)) {
+        points.add(this.right!!)
+    }
+
+    return points
+}
+
+private fun Point?.isBasinPoint(adjacentHeight: Int) =
+    this != null &&
+    this.height < 9 &&
+    this.height > adjacentHeight
+
+data class Point(val x: Int, val y: Int, val height: Int)
+
+data class Adjacent(
+    val point: Point,
+    val top: Point?,
+    val left: Point?,
+    val bottom: Point?,
+    val right: Point?
+)
