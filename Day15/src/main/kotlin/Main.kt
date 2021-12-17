cad1cf4f2c4ed85238bb9908a137795785520a46
Matt <matt_swarbrick@hotmail.com>
2021-12-17 17:43:52 +0000

    Day 15 - part 1 solution

--- Day15/src/main/kotlin/Main.kt
+++ Day15/src/main/kotlin/Main.kt
@@ -5,22 +5,8 @@ fun main() {
 
 private var maxX: Int = 0
 private var maxY: Int = 0
-private var currentLowestRisk = 1000
 
 private fun solvePart1() {
-    val input = listOf(
-        "1163751742",
-        "1381373672",
-        "2136511328",
-        "3694931569",
-        "7463417111",
-        "1319128137",
-        "1359912421",
-        "3125421639",
-        "1293138521",
-        "2311944581"
-    )
-
     val answer = puzzle
         .toPoints()
         .toPaths()
@@ -47,6 +33,41 @@ private fun solvePart2() {
     println("Solution to part2: $answer")
 }
 
+private fun List<Point>.toPaths(): List<List<Point>> {
+    val startingPoint = this.first { it.isStartPoint() }
+    val startingPath = listOf(startingPoint)
+    val risks = mutableMapOf<Point, Int>()
+    return startingPath.toPaths(this, risks)
+}
+
+private fun List<Point>.toPaths(allPoints: List<Point>, risks: MutableMap<Point, Int>): List<List<Point>> =
+    this.last()
+        .toNeighboursDown(allPoints)
+        .map { this.plus(it) }
+        .flatMap {
+            val lastPoint = it.last()
+            val risk = it.toRisk()
+            val currentLowRisk = risks[lastPoint]
+            if(currentLowRisk != null && risk < currentLowRisk){
+                risks[lastPoint] = risk
+            } else if(currentLowRisk == null) {
+                risks[lastPoint] = risk
+            }
+
+            if((currentLowRisk != null && risk > currentLowRisk) || this.contains(lastPoint)) {
+                listOf()
+            } else if(lastPoint.isEndPoint())  {
+                listOf(it)
+            } else {
+                it.toPaths(allPoints, risks)
+            }
+        }
+
+
+private fun List<Point>.toDiagonal(allPoints: List<Point>): List<Point> =
+    this.flatMap { it.toNeighboursUp(allPoints) }.distinct()
+
+
 private fun List<String>.toPoints(): List<Point> {
     maxX = this[0].lastIndex
     maxY = this.lastIndex
@@ -58,47 +79,29 @@ private fun List<String>.toPoints(): List<Point> {
     }.flatten()
 }
 
-private fun List<Point>.toPaths(): Set<List<Point>> {
-    val startingPoint = this.first { it.isStartPoint() }
-    val startingPath = listOf(startingPoint)
-    return startingPath.toPaths(this)
-}
-
-private fun List<Point>.toPaths(allPoints: List<Point>): Set<List<Point>> =
-    this.last()
-        .toNeighbours(allPoints)
-        .map { this.plus(it) }
-        .flatMap {
-            val lastPoint = it.last()
-            val risk = it.toRisk()
-            if(risk > currentLowestRisk || this.contains(lastPoint)) {
-                listOf()
-            } else if(lastPoint.isEndPoint())  {
-                if(risk < currentLowestRisk) {
-                    currentLowestRisk = risk
-                }
-                setOf(it)
-            } else {
-                it.toPaths(allPoints)
-            }
-        }.toSet()
+private fun List<List<Point>>.toLowestRisk() =
+    this.minOf { it.filter { point -> !point.isStartPoint() }.toRisk() }
 
 private fun List<Point>.toRisk() = this.sumOf { it.risk }
-
-private fun Set<List<Point>>.toLowestRisk() =
-    this.minOf { it.filter { point -> !point.isStartPoint() }.toRisk() }
 
 private fun Point.isEndPoint() = this.x == maxX && this.y == maxY
 private fun Point.isStartPoint() = this.x == 0 && this.y == 0
 
-private fun Point.toNeighbours(allPoints: List<Point>): List<Point> {
+private fun Point.toNeighboursUp(allPoints: List<Point>): List<Point> {
     val top = allPoints.find { it.x == this.x && it.y == this.y - 1 }
     val left = allPoints.find { it.x == this.x - 1 && it.y == this.y }
-    val right = allPoints.find { it.x == this.x + 1 && it.y == this.y }
-    val bottom = allPoints.find { it.x == this.x && it.y == this.y + 1 }
 
     return listOfNotNull(
-        top, left, right, bottom
+        top, left
+    )
+}
+
+private fun Point.toNeighboursDown(allPoints: List<Point>): List<Point> {
+    val bottom = allPoints.find { it.x == this.x && it.y == this.y + 1 }
+    val right = allPoints.find { it.x == this.x + 1 && it.y == this.y }
+
+    return listOfNotNull(
+        bottom, right
     )
 }
 
