99169ca1f9fa613014f4c9ee1970a24ede5bd2fe
Matt Swarbrick <matt@invevo.com>
2021-12-21 16:17:42 +0000

    Day 18 - part 2 solution

--- Day18/src/main/kotlin/Main.kt
+++ Day18/src/main/kotlin/Main.kt
@@ -16,19 +16,8 @@ private fun solvePart1() {
 }
 
 private fun solvePart2() {
-    val input = listOf(
-        "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
-        "[[[5,[2,8]],4],[5,[[9,9],0]]]",
-        "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
-        "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
-        "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
-        "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
-        "[[[[5,4],[7,7]],8],[[8,3],8]]",
-        "[[9,3],[[9,9],[6,[4,9]]]]",
-        "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
-        "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
-    )
-    val answer = 0
+    val answer = puzzle.toSnailPairs()
+        .toLargestMagnitude()
 
     println("Solution to part2: $answer")
 }
@@ -119,6 +108,14 @@ fun SnailPair.toMagnitude(): Long {
 
     return leftValue + rightValue
 }
+
+private fun List<SnailPair>.toLargestMagnitude(): Long =
+    this.flatMap { leftSideSnailPair ->
+        this.filter { it != leftSideSnailPair }
+            .map { rightSideSnailPair ->
+                listOf(leftSideSnailPair, rightSideSnailPair).toSum().toMagnitude()
+            }
+    }.maxOrNull()!!
 
 private fun List<SnailPair>.toMagnitude(): Long =
     this.last().toMagnitude()
